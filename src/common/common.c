158b0090b26305fc86fa893fb6578f147535e44b
Soar Qin <soarchin@gmail.com>
2026-04-25 03:16:54 +0800

    chore(common): scaffold src/common/ static library skeleton

---
+++ src/common/common.c
@@ -0,0 +1,9 @@
+/**
+ * @file common.c
+ * @brief Placeholder translation unit for the ersave_common static library.
+ * @details Real modules (ersave, save_compress, file_dialog, locale_core, config_core)
+ *          are added in Wave 2 tasks T4-T8.
+ */
+
+/* Placeholder: suppress empty-translation-unit warning */
+typedef int ersave_common_placeholder_t;
